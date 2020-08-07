6e480f43b52724c45e8e18975b860ca2a1e3aba2
ZoheirGeneral <39691399+Zoheirgeneral@users.noreply.github.com>
2020-08-07 21:40:40 +0430

    accounts

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, HttpResponse
 from . import models
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def articles_list(request):
-    articles = models.Article.objects.all().order_by('date')
+    articles = models.Article.objects.all().order_by('-date')
     args = {'articles':articles}
     return render(request, 'articles/articleslist.html', args)
 
 def article_details(request,slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slugg)
+    article = models.Article.objects.get(slug=slug)
+    return render(request, 'articles/article_descriptions.html', {'article':article})
+
+@login_required(login_url="/accounts/login")
+def create_article(request):
+    return render(request, 'articles/create_article.html')
